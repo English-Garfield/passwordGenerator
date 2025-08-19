acadd1b49a66306cfc48a221b5ea05e0b4d93370
Isaac Korda <104942741+English-Garfield@users.noreply.github.com>
2025-08-19 09:10:23 +0100

    first few classes and functions

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import secrets
+import string
+import argparse
+import json
+import sys
+from typing import List, Dict, Any
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class passwordGenerator:
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __init__(self):
+        pass
+
+    def generate_password(self):
+        pass
+
+    def calculateEntropy(self):
+        pass
+
+    def assessStrength(self):
+        pass
+
+    def generateMultiple(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
